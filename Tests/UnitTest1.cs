96be70d2ccb08060f4fe7523896e9085cd49b93c
Madden <AL07839@LDD70HMQQXX.US.AD.WELLPOINT.COM>
2022-10-09 13:20:04 +0100

    adding unit tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BMICalculator;
+
+namespace Tests
+{
+    [TestClass]
+    public class BMICalc_Verify
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BMI bmi = new BMI { WeightStones = 15, WeightPounds = 10, HeightFeet = 6, HeightInches = 2 };
+            Assert.AreNotEqual(bmi.BMICategory, BMICategory.Normal);
+        }
+        public void TestMethod2()
+        {
+            BMI bmi = new BMI { WeightStones = 12, WeightPounds = 10, HeightFeet = 6, HeightInches = 2 };
+            Assert.AreEqual(bmi.BMICategory, BMICategory.Normal);
+        }
+    }
+}
